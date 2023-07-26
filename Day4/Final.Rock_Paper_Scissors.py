5a0bf6b775e1b790655de7eb2ad5840e9cc7c1fd
mohamadreza bigdeli <bigdelim80@gmail.com>
2023-07-26 08:47:07 +0330

    Add Day4

--- Day4/Final.Rock_Paper_Scissors.py
+++ Day4/Final.Rock_Paper_Scissors.py
@@ -31,7 +31,7 @@ print('''
       ''')
 
 print("Welcome to Rock, Paper, Scissors game.Each player reach score 3 sonner, is winner. Have fun!")
-human_score = 0
+user_score = 0
 computer_score = 0
 
 rock = ('''
@@ -63,19 +63,19 @@ scissors = ('''
 choices_list = ["Rock", "Paper", "Scissors"]
 
 while True:
-    if human_score >= 3 and human_score > computer_score:
-        print(f"You: {human_score}\nComputer: {computer_score}\nYou Win!")
+    if user_score >= 3 and user_score > computer_score:
+        print(f"You: {user_score}\nComputer: {computer_score}\nYou Win!")
         break
-    if computer_score >= 3 and computer_score > human_score:
-        print(f"You: {human_score}\nComputer: {computer_score}\nGame Over!")
+    if computer_score >= 3 and computer_score > user_score:
+        print(f"You: {user_score}\nComputer: {computer_score}\nGame Over!")
         break
 
-    human_choice = input(
+    user_choice = input(
         "What do you choose? Type 0 for Rock, Type 1 for Paper, Type 2 for Scissors\nType 'quit' for exit:\n")
-    if human_choice == "quit":
+    if user_choice == "quit":
         break
 
-    human_choice = int(human_choice)
+    user_choice = int(user_choice)
     computer_choice = random.randint(0, len(choices_list) - 1)
 
     if computer_choice == 0:
@@ -85,25 +85,25 @@ while True:
     elif computer_choice == 2:
         print(f"Computer: {choices_list[2]}{scissors}")
 
-    if human_choice == 0:
+    if user_choice == 0:
         print(f"You: {choices_list[0]}{rock}")
-    elif human_choice == 1:
+    elif user_choice == 1:
         print(f"You: {choices_list[1]}{paper}")
-    elif human_choice == 2:
+    elif user_choice == 2:
         print(f"You: {choices_list[2]}{scissors}")
     else:
-        print("Option is not available. Type 0 for Rock\nType 1 for Paper\nType 2 for Scissors\n")
+        print("Option is not available. Type 0 for Rock, Type 1 for Paper,Type 2 for Scissors!")
         continue
 
-    if computer_choice == human_choice:
-        print(f"You: {human_score}\nComputer: {computer_score}")
+    if computer_choice == user_choice:
+        print(f"You: {user_score}\nComputer: {computer_score}")
 
-    elif (computer_choice == 0 and human_choice == 2) or (computer_choice == 1 and human_choice == 0) or \
-            (computer_choice == 2 and human_choice == 1):
+    elif (computer_choice == 0 and user_choice == 2) or (computer_choice == 1 and user_choice == 0) or \
+            (computer_choice == 2 and user_choice == 1):
         computer_score += 1
-        print(f"You: {human_score}\nComputer: {computer_score}")
+        print(f"You: {user_score}\nComputer: {computer_score}")
 
-    elif (human_choice == 0 and computer_choice == 2) or (human_choice == 1 and computer_choice == 0) or \
-            (human_choice == 2 and computer_choice == 1):
-        human_score += 1
-        print(f"You: {human_score}\nComputer: {computer_score}")
+    elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or \
+            (user_choice == 2 and computer_choice == 1):
+        user_score += 1
+        print(f"You: {user_score}\nComputer: {computer_score}")
